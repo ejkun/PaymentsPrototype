4f3f1092fba3311cf992728c89e0053b751c4905
Everton Barbosa Jr <everton.barbosa@convenia.com.br>
2020-12-04 16:36:43 -0300

    Add Unit and Features tests for User

--- tests/Feature/Controller/UserControllerTest.php
+++ tests/Feature/Controller/UserControllerTest.php
@@ -1,10 +1,116 @@
 <?php
 
-
 namespace Tests\Feature\Controller;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
 
-class UserControllerTest
+class UserControllerTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
 
+    public function testApiIndexUsers()
+    {
+        $users = User::factory()->count(5)->create();
+
+        $response = $this->getJson(route('users.index'));
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $users->toArray()
+            ]);
+    }
+
+    public function testApiShowsAUser()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->getJson(route('users.show', ['user' => $user->id]));
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $user->toArray()
+            ]);
+    }
+
+    public function testApiStoresAUser()
+    {
+        $password = $this->faker->password;
+        $user = User::factory()->make([])
+            ->toArray();
+        unset($user['balance']);
+
+        $user['password'] = $password;
+        $user['password_confirmation'] = $password;
+
+        $response = $this->postJson(route('users.store'), $user);
+
+        unset($user['password'], $user['password_confirmation']);
+
+        $response->assertStatus(Response::HTTP_CREATED)
+            ->assertJson($user);
+
+        $this->assertDatabaseHas('users', array_merge(
+            ['id' => $response->json('id')],
+            $user
+        ));
+    }
+
+    public function testApiUpdatesAUser()
+    {
+        $password = $this->faker->password;
+        $cryptedPassword = Hash::make($password);
+        $user = User::factory()->create([
+            'password' => $cryptedPassword
+        ]);
+
+        $newName = $this->faker->name;
+
+        $response = $this
+            ->actingAs($user)
+            ->putJson(
+                route('users.update', ['user' => $user->id]),
+                [
+                    'name' => $newName,
+                    'current_password' => $password
+                ]
+            );
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => $newName
+        ]);
+    }
+
+    public function testApiDeletesAUser()
+    {
+        $password = $this->faker->password;
+        $cryptedPassword = Hash::make($password);
+        $user = User::factory()->create([
+            'password' => $cryptedPassword
+        ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->deleteJson(
+                route('users.destroy', ['user' => $user->id]),
+                [
+                    'current_password' => $password
+                ]
+            );
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id
+        ]);
+    }
 }
--- tests/Unit/Services/UserServiceTest.php
+++ tests/Unit/Services/UserServiceTest.php
@@ -1,7 +1,68 @@
 <?php
 
+namespace Tests\Unity\Services\UserServiceTest;
 
-class UserServiceTest
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
 {
+    use RefreshDatabase;
 
+    protected UserService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->service = app(UserService::class);
+    }
+
+    public function testItIndexUsers()
+    {
+        $users = User::factory()->count(5)->create();
+
+        $indexedUsers = $this->service->index();
+
+        $this->assertEquals($users->toArray(), $indexedUsers->toArray());
+    }
+
+    public function testItStoresAUser()
+    {
+        $userData = User::factory()->definition();
+
+        $user = $this->service->store($userData);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertDatabaseHas('users', $user->toArray());
+    }
+
+    public function testItUpdatesAUser()
+    {
+        $user = User::factory()->create();
+        $data = User::factory()->make()
+            ->toArray();
+
+        unset($data['email_verified_at']);
+
+        $updated = $this->service->update($user, $data);
+
+        $this->assertTrue($updated);
+        $this->assertDatabaseHas('users', array_merge(
+            ['id' => $user->id],
+            $data
+        ));
+    }
+
+    public function testItDestroysAUser()
+    {
+        $user = User::factory()->create();
+
+        $deleted = $this->service->destroy($user);
+
+        $this->assertTrue($deleted);
+        $this->assertDatabaseMissing('users', $user->toArray());
+    }
 }
