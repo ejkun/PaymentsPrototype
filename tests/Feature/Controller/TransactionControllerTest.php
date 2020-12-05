7cf66fb1601fd320fe3ad2ddc3e41f964fa1435d
Everton Barbosa Jr <everton.barbosa@convenia.com.br>
2020-12-05 01:48:40 -0300

    Add Unit and Features tests for Transaction

--- tests/Feature/Controller/TransactionControllerTest.php
+++ tests/Feature/Controller/TransactionControllerTest.php
@@ -1,10 +1,94 @@
 <?php
 
-
 namespace Tests\Feature\Controller;
 
+use App\Exceptions\InsufficientFundsException;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
 
-class TransactionControllerTest
+class TransactionControllerTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
 
+    private User $payee;
+    private User $payer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->payer = User::factory()->create();
+        $this->payee = User::factory()->create();
+    }
+
+    public function testApiIndexTransactions()
+    {
+        $transactions = Transaction::factory()->count(5)->create([
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id
+        ])->toArray();
+
+        array_walk($transactions, function (array &$transaction) {
+            unset($transaction['updated_at']);
+        });
+
+        $response = $this->getJson(route('transactions.index'));
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+            'data' => $transactions
+        ]);
+    }
+
+    public function testApiStoresATransaction()
+    {
+        $value = $this->faker->randomFloat(2, 1, $this->payer->balance);
+
+        $data = [
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id,
+            'value' => $value
+        ];
+
+        $response = $this->postJson(
+            route('transactions.store'),
+            $data
+        );
+
+        $response->assertJson($data);
+        $this->assertDatabaseHas('transactions', array_merge(
+            ['id' => $response->json('id')],
+            $data
+        ));
+    }
+
+    public function testApiThrowsBadRequestWithInsufficientFunds()
+    {
+        $value = $this->payer->balance + 1;
+
+        $data = [
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id,
+            'value' => $value
+        ];
+
+        $response = $this->postJson(
+            route('transactions.store'),
+            $data
+        );
+
+        $response
+            ->assertStatus(Response::HTTP_BAD_REQUEST);
+
+        $this->assertInstanceOf(InsufficientFundsException::class, $response->exception);
+        $this->assertDatabaseMissing('transactions', array_merge(
+            ['id' => $response->json('id')],
+            $data
+        ));
+    }
 }
--- tests/Unit/Services/TransactionServiceTest.php
+++ tests/Unit/Services/TransactionServiceTest.php
@@ -1,10 +1,80 @@
 <?php
 
-
 namespace Tests\Unity\Services\UserServiceTest;
 
+use App\Exceptions\InsufficientFundsException;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Services\TransactionService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
-class TransactionServiceTest
+class TransactionServiceTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
 
+    private TransactionService $service;
+    private User $payee;
+    private User $payer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->payer = User::factory()->create();
+        $this->payee = User::factory()->create();
+        $this->service = app(TransactionService::class);
+    }
+
+    public function testItIndexTransactions()
+    {
+        $transactions = Transaction::factory()->count(5)->create([
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id
+        ]);
+
+        $this->assertEquals(
+            $this->service->index()->toArray(),
+            $transactions->toArray()
+        );
+    }
+
+    public function testItStoresATransaction()
+    {
+        $value = $this->faker->randomFloat(2, 1, $this->payer->balance);
+
+        $transaction = $this->service->store($this->payer, $this->payee, $value);
+
+        $expectedData = [
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id,
+            'value' => $value
+        ];
+
+        $this->assertInstanceOf(Transaction::class, $transaction);
+
+        array_walk($expectedData, function ($value, $key) use ($transaction) {
+            $this->assertEquals($value, $transaction->$key);
+        });
+        $this->assertDatabaseHas('transactions', $expectedData);
+    }
+
+    public function testItThrowsInsufficientFundsException()
+    {
+        $value = $this->payer->balance + 1;
+
+        try {
+            $this->service->store($this->payer, $this->payee, $value);
+        } catch (InsufficientFundsException $exception) {
+            $this->assertTrue(true, 'This line was not called');
+        }
+
+        $this->assertDatabaseMissing('transactions', [
+            'payer_id' => $this->payer->id,
+            'payee_id' => $this->payee->id,
+            'value' => $value
+        ]);
+    }
 }
